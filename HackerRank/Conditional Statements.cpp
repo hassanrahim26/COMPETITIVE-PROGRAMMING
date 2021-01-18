daf3ae0123945be8c553438023d9eb58b5e741aa
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-01-18 20:00:06 +0530

    Create Conditional Statements.cpp

---
+++ HackerRank/Conditional Statements.cpp
@@ -0,0 +1,43 @@
+/*
+PROBLEM LINK: https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem
+*/
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll n;
+    cin >> n;
+    if(n==1){
+        cout << "one" << endl;
+    }
+    else if(n==2){
+        cout << "two" << endl;
+    }
+    else if(n==3){
+        cout << "three" << endl;
+    }
+    else if(n==4){
+        cout << "four" << endl;
+    }
+    else if(n==5){
+        cout << "five" << endl;
+    }
+    else if(n==6){
+        cout << "six" << endl;
+    }
+    else if(n==7){
+        cout << "seven" << endl;
+    }
+    else if(n==8){
+        cout << "eight" << endl;
+    }
+    else if(n==9){
+        cout << "nine" << endl;
+    }
+    else if(n>9){
+        cout << "Greater than 9" << endl;
+    }
+    return 0;
+}
