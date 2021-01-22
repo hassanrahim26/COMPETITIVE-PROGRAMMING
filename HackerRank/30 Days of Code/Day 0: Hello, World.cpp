e0af951fba3b649b7c8ed1758f7f1f8e4fef0302
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-01-22 14:26:15 +0530

    Create Day 0: Hello, World.cpp

---
+++ HackerRank/30 Days of Code/Day 0: Hello, World.cpp
@@ -0,0 +1,18 @@
+/*
+PROBLEM LINK: https://www.hackerrank.com/challenges/30-hello-world/problem
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin,s);
+    cout << "Hello, World.\n" << s;
+    return 0;
+}
+
