30f7187fe7424a07405e2784929f3a2049ab8e6a
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-01-23 18:40:48 +0530

    Create Mahmoud and Ehab and the even-odd game.cpp

---
+++ CODEFORCES/Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,17 @@
+/*
+PROBLEM LINK: https://codeforces.com/contest/959/problem/A
+*/
+
+#include<iostream>
+#define ll long long
+using namespace std;
+ 
+int main(){
+	ll n;
+	cin >> n;
+	if(n%2==0)
+		cout << "Mahmoud";
+	else
+		cout << "Ehab";
+	return 0;
+}
