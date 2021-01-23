0eed82ace10689aa3904f7502741a2a332d2d392
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-01-23 18:46:39 +0530

    Create Floor Number.cpp

---
+++ CODEFORCES/Floor Number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+int main(){
+	ll t,n,x;
+	cin >> t;
+	while(t--){
+		cin >> n >> x;
+		if(n<=2)
+			cout << 1 << endl;
+		if(n>2)
+			cout << ceil((n-3)/x) + 2 << endl;
+	}
+	return 0;
+}
