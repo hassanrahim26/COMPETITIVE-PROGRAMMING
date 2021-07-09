1a96ffbd6cf243fa1077929e022e43b471c30487
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-07-09 17:43:03 +0530

    Create Odd Set.cpp

---
+++ CODEFORCES/Odd Set.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+int main()
+{
+    ll t, n, i;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int y = 0;
+        for (i = 1; i <= 2 * n; i++)
+        {
+            int x;
+            cin >> x;
+            if (x % 2)
+                y++;
+        }
+        if (y == n)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    return 0;
+}
