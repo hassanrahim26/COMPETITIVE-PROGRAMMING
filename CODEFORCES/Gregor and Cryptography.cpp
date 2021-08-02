06bd2611a5ae79a6a9465c7a09f449ae36a7391e
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-08-02 10:17:59 +0530

    Create Gregor and Cryptography.cpp

---
+++ CODEFORCES/Gregor and Cryptography.cpp
@@ -0,0 +1,22 @@
+/*
+PROBLEM LINK:- https://codeforces.com/contest/1549/problem/0
+*/
+
+#include<bits/stdc++.h>
+#define ll long long
+#define test ll t; cin >> t; while(t--)
+#define FOR for(int i = 0; i < n; i++)
+#define nn "\n"
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+    test
+    {
+        ll p;
+        cin >> p;
+        cout << 2 << " " << p - 1 << nn;
+    }
+    return 0;
+}
