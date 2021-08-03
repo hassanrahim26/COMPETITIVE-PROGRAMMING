d34d29ee2cca0c90cd6561aa6b5a98f54376b49b
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-08-03 15:47:10 +0530

    Create EhAb AnD gCd.cpp

---
+++ CODEFORCES/EhAb AnD gCd.cpp
@@ -0,0 +1,22 @@
+/*
+PROBLEM LINK:- https://codeforces.com/problemset/problem/1325/A
+*/
+
+#include<bits/stdc++.h>
+#define ll long long
+#define test ll t; cin >> t; while(t--)
+#define FOR for(int i = 0; i < n; i++)
+#define nn "\n"
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+    test
+    {
+        ll x;
+        cin >> x;
+        cout << 1 << " " << x - 1 << nn;
+    }
+    return 0;
+}
