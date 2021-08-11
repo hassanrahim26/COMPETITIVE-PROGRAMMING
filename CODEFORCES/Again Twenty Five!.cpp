baa71bd1ab6d0aee766cba411efceb4146ef4fdc
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-08-11 11:05:08 +0530

    Create Again Twenty Five!.cpp

---
+++ CODEFORCES/Again Twenty Five!.cpp
@@ -0,0 +1,18 @@
+/*
+PROBLEM LINK:- https://codeforces.com/problemset/problem/630/A
+*/
+
+#include<bits/stdc++.h>
+#define ll long long
+#define test ll t; cin >> t; while(t--)
+#define nn "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+    ll n;
+    cin >> n;
+    cout << 25 << nn;
+    return 0;
+}
