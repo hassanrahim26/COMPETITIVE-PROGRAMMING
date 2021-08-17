a85e745758084c994f3496e5b8242c4418ad076d
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-08-17 10:24:48 +0530

    Create Remove a Progression.cpp

---
+++ CODEFORCES/Remove a Progression.cpp
@@ -0,0 +1,21 @@
+/* 
+PROBLEM LINK:- https://codeforces.com/contest/1194/problem/A
+*/
+
+#include<bits/stdc++.h>
+#define ll long long
+#define test ll t; cin >> t; while(t--)
+#define nn "\n"
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(0);
+	test
+	{
+		ll n, x;
+		cin >> n >> x;
+		cout << 2 * x << nn;
+	}
+	return 0;
+}
