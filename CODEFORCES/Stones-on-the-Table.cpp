303bd36de53150a4cfe075d0cdae2048df777b66
HASSAN RAHIM <hassanrahim26@gmail.com>
2022-02-08 19:37:49 +0530

    Update Stones-on-the-Table.cpp

--- CODEFORCES/Stones-on-the-Table.cpp
+++ CODEFORCES/Stones-on-the-Table.cpp
@@ -6,24 +6,21 @@ PROBLEM LINK:- https://codeforces.com/problemset/problem/266/A
 #define ll long long
 #define FOR for(ll i = 0; i < n; i++)
 using namespace std;
-
+ 
 int main()
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	ll n, count = 0;
+	ll n, c = 0;
 	cin >> n;
-	char c[n];
+	string s;
+	cin >> s;
+ 
 	FOR
 	{
-		cin >> c[i];
+		if (s[i] == s[i + 1])
+			c++;
 	}
-
-	for (int i = 1; i < n; i++)
-	{
-		if (c[i - 1] == c[i])
-			count++;
-	}
-
-	cout << count;
+ 
+	cout << c;
 	return 0;
 }
