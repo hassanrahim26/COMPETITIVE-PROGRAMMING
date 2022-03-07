60604e250b21e9bdec6a909f6170e322f9558d7b
HASSAN RAHIM <hassanrahim26@gmail.com>
2022-03-07 22:34:00 +0530

    Create Square-Counting.cpp

---
+++ CODEFORCES/Square-Counting.cpp
@@ -0,0 +1,19 @@
+/*
+PROBLEM LINK:- https://codeforces.com/problemset/problem/1646/A
+*/
+
+#include<bits/stdc++.h>
+#define ll long long
+#define test ll t; cin >> t; while(t--)
+using namespace std;
+
+int main()
+{
+	test
+	{
+		ll n, s;
+		cin >> n >> s;
+		cout << s / (n * n) << endl;
+	}
+	return 0;
+}
